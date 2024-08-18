5f0244d4a3b20d9e902b58e54166d0fe1f54ad83
fabiano <jeovajire2003@gmail.com>
2024-08-17 22:32:57 -0300

    Splitec code in reorderableListWidget.dart

--- lib/modules/home/widgets/reorderableListWidget.dart
+++ lib/modules/home/widgets/reorderableListWidget.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: file_names
-
 import 'package:flutter/material.dart';
 
 class ReorderableListWidget extends StatefulWidget {
@@ -29,54 +28,84 @@ class ReorderableListWidgetState extends State<ReorderableListWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final bool orientation = _verticalOrientation;
-    List<Widget> childrenList = [];
-
-    if (orientation) {
-      childrenList = _reorderedList.asMap().entries.map(
-        (entry) {
-          final index = entry.key;
-          final widget = entry.value;
-          return ListTile(
-            key: Key('$index'),
-            title: widget,
-            tileColor: Colors.transparent,
+    return _verticalOrientation
+        ? ReorderableListVertical(
+            reorderedList: _reorderedList,
+            onReorder: _onReorder,
+          )
+        : ReorderableListHorizontal(
+            reorderedList: _reorderedList,
+            onReorder: _onReorder,
           );
-        },
-      ).toList();
-    } else {
-      ListView(
-        scrollDirection: Axis.horizontal,
-        children: _reorderedList.asMap().entries.map(
-          (entry) {
-            final index = entry.key;
-            final widget = entry.value;
+  }
 
-            return SizedBox(
-              width: 150,
-              child: ListTile(
-                key: Key('$index'),
-                title: widget,
-                tileColor: Colors.transparent,
-              ),
-            );
-          },
-        ).toList(),
+  void _onReorder(int oldIndex, int newIndex) {
+    setState(() {
+      if (newIndex > oldIndex) {
+        newIndex -= 1;
+      }
+      final Widget item = _reorderedList.removeAt(oldIndex);
+      _reorderedList.insert(newIndex, item);
+    });
+  }
+}
+
+class ReorderableListVertical extends StatelessWidget {
+  final List<Widget> reorderedList;
+  final void Function(int, int) onReorder;
+
+  const ReorderableListVertical({
+    super.key,
+    required this.reorderedList,
+    required this.onReorder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final childrenList = reorderedList.asMap().entries.map((entry) {
+      final index = entry.key;
+      final widget = entry.value;
+      return ListTile(
+        key: Key('$index'),
+        title: widget,
+        tileColor: Colors.transparent,
       );
-    }
+    }).toList();
 
     return ReorderableListView(
       buildDefaultDragHandles: true,
-      onReorder: (oldIndex, newIndex) {
-        setState(() {
-          if (newIndex > oldIndex) {
-            newIndex -= 1;
-          }
-          final Widget item = _reorderedList.removeAt(oldIndex);
-          _reorderedList.insert(newIndex, item);
-        });
-      },
+      onReorder: onReorder,
       children: childrenList,
+    );
+  }
+}
+
+class ReorderableListHorizontal extends StatelessWidget {
+  final List<Widget> reorderedList;
+  final void Function(int, int) onReorder;
+
+  const ReorderableListHorizontal({
+    super.key,
+    required this.reorderedList,
+    required this.onReorder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: reorderedList.asMap().entries.map((entry) {
+        final index = entry.key;
+        final widget = entry.value;
+        return SizedBox(
+          width: 150,
+          child: ListTile(
+            key: Key('$index'),
+            title: widget,
+            tileColor: Colors.transparent,
+          ),
+        );
+      }).toList(),
     );
   }
 }
